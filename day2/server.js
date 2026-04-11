68e9b85da3623429681168cf574b8857313d319d
samyakjain09 <samyakjain1p@gmail.com>
2026-04-11 23:49:27 +0530

    backend basics

--- day2/server.js
+++ day2/server.js
@@ -4,5 +4,11 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
+app.get('/about',(req,res)=>{
+    res.send("hello world")
+})
+app.get('/courses',(req,res)=>{
+    res.send("hello world")
+})
 
 app.listen(3000)
