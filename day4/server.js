fbb29fc06254d100e4537ea2f829c520b6b1c7ce
samyakjain09 <samyakjain1p@gmail.com>
2026-04-15 04:29:42 +0530

    notes project

--- day4/server.js
+++ day4/server.js
@@ -1,18 +1,8 @@
 const express= require("express")
 const app=express()
 
-
-const notes = [
-    {
-        title:"test1",
-        description:"description1"
-    }
-]
-
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
 
-
-
-module.exports=app
+app.listen(3000)
