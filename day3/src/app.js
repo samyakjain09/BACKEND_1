c5033bcde23a035c757423fc9dbd32fa0e581720
samyakjain09 <samyakjain1p@gmail.com>
2026-04-16 03:27:29 +0530

    day 4

--- day3/src/app.js
+++ day3/src/app.js
@@ -1,16 +1,36 @@
 const express= require("express")
 const app=express()
 
+app.use(express.json())
+
 
 const notes = [
-    {
-        title:"test1",
-        description:"description1"
-    }
+   
 ]
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
+
+
+app.post("/notes",(req,res)=>{
+    console.log(req.body)
+    notes.push(req.body)
+    console.log(notes)
+    res.send("note created")
+})
+
+app.get("/notes",(req,res)=>{
+    res.send(notes)
+})
+
+
+app.delete("/notes/:index",(req,res)=>{
+    delete notes[req.params.index]
+    console.log("notes deleted successfully")
+})
+
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].description=req.body.test_description
+    console.log("description updated succesfully")
+    
 })
 
 
