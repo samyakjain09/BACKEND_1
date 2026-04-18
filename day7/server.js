1a97ee9ab6ebcd73b6bbcd7692ba68c9737dfa1c
samyakjain09 <samyakjain1p@gmail.com>
2026-04-19 04:03:25 +0530

    Crud oprations

---
+++ day7/server.js
@@ -0,0 +1,9 @@
+const app = require("./src/app")
+
+app.get("/",(req,res)=>{
+    res.send("hello samyak this side")
+})
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
