eab5852348970688f52c934e150fbabeb5179aaa
samyakjain09 <samyakjain1p@gmail.com>
2026-04-20 23:07:57 +0530

    crud

--- day7/server.js
+++ day7/server.js
@@ -7,3 +7,13 @@ app.get("/",(req,res)=>{
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
+
+const app = require("./src/app")
+
+app.get("/",(req,res)=>{
+    res.send("hello samyak this side")
+})
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
