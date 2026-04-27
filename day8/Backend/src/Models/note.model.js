8c36e8856580ba707bc82d9c38bf890cc98b899c
samyakjain09 <samyakjain1p@gmail.com>
2026-04-27 23:35:14 +0530

    database

--- day8/Backend/src/Models/note.model.js
+++ day8/Backend/src/Models/note.model.js
@@ -5,6 +5,11 @@ const noteSchema= new mongoose.Schema({
     description:String
 })
 
+const noteSchema= new mongoose.Schema({
+    title:String,
+    description:String
+})
+
 const noteModel=mongoose.model("notes",noteSchema)
 
 module.exports=noteModel
