3033dda77fdd80b89e1a67d65a2dfbc2f06f8d7d
samyakjain09 <samyakjain1p@gmail.com>
2026-05-12 21:39:09 +0530

    day9

---
+++ day9/server.js
@@ -0,0 +1,7 @@
+const app=require("./src/app")
+require("dotenv").config
+const connectToDb=require("./src/config/database")
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
