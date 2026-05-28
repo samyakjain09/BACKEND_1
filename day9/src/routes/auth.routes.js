f301b3ff3766609a580b189811c0e5f812cd5190
samyakjain09 <samyakjain1p@gmail.com>
2026-05-28 23:09:00 +0530

    day9

--- day9/src/routes/auth.routes.js
+++ day9/src/routes/auth.routes.js
@@ -1,17 +1,38 @@
 const express = require("express")
 const userModel=require("../models/user.model")
-
+const jwt= require("jsonwebtoken")
 const authRouter=express.Router()
 
 authRouter.post("/register",async(req,res)=>{
     const {email,name,password} = req.body
+
+    const isUserAlreadyExists=await userModel.findOne({email})
+
+    if(isUserAlreadyExists){
+        return res.status(400).json({
+            "message":"Email already Exists"
+        })
+    }
+
+
     const user=await userModel.create({
         email,password,name
     })
 
+    const token=jwt.sign(
+        {
+            id:user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token",token)
+
+
     res.status(201).json({
         message:"user registered successfully",
-        user
+        user,token
     })
 })
 
