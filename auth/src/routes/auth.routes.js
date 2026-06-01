b0789057a99947a2ff6be50e953071060f31495f
samyakjain09 <samyakjain1p@gmail.com>
2026-06-01 10:48:05 +0530

    auth complete

--- auth/src/routes/auth.routes.js
+++ auth/src/routes/auth.routes.js
@@ -34,6 +34,44 @@ authRouter.get("/get-me",async(req,res)=>{
     const decoded=jwt.verify(token,process.env.JWT_SECRET)
     console.log(decoded )
     const user=await userModel.findById(decoded.id)
+    res.json({
+        "name":user.name,
+        "email":user.email
+    })
+})
+
+authRouter.post("/login",async(req,res)=>{
+    const {email,password}=req.body
+
+    const user= await userModel.findOne({email})
+    if(!user){
+        res.status(404).json({
+            "message":"user not found"
+        })
+    }
+
+    const hash = crypto.createHash('sha256').update(password).digest('hex')
+
+    const isPasswordCorrect=hash==user.password
+
+    if(!isPasswordCorrect){
+        return res.status(401).json({
+            "message":"Incorrect Password"
+        })
+    }
+
+    const token=jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET,{expiresIn:"1h"})
+    res.cookie("token",token)
+
+    res.json({
+        "message":"user logged in successfully",
+        user:{
+            name:user.name,
+            email:user.email
+        }
+    })
 })
 
 module.exports=authRouter
