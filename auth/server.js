e5dff64681b92941a8d57d546c751f19e89c5fb2
samyakjain09 <samyakjain1p@gmail.com>
2026-06-26 23:25:31 +0530

    Remove authentication

--- auth/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const connectDB=require("./src/config/database")
-
-connectDB()
-
-const app=require("./src/app")
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})
