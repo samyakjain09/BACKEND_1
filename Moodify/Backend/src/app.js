91e1e0196a14018c1d6002882694f4d63f37cfb8
samyakjain09 <samyakjain1p@gmail.com>
2026-06-29 01:11:07 +0530

    moodify complete

--- Moodify/Backend/src/app.js
+++ Moodify/Backend/src/app.js
@@ -3,6 +3,7 @@ const cookieParser=require("cookie-parser")
 const authRoutes=require("./routes/auth.routes")
 const cors=require("cors")
 const songRoutes=require("./routes/song.routes")
+const path = require("path");
 
 const app=express()
 app.use(express.json())
@@ -14,5 +15,12 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes)
 app.use("/api/songs",songRoutes)
+
+app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
+
+app.get("*", (req, res, next) => {
+  if (req.path.startsWith("/api/")) return next();
+  res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
+});
 
 module.exports=app
