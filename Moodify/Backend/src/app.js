52aa5852b0fecf5b480b656ad9d4a67f36b2e2df
samyakjain09 <samyakjain1p@gmail.com>
2026-06-30 12:25:02 +0530

    refactor CORS configuration for improved readability

--- Moodify/Backend/src/app.js
+++ Moodify/Backend/src/app.js
@@ -9,9 +9,12 @@ const app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://moodify-1zy5.onrender.com"
+  ],
+  credentials: true
+}));
 
 app.use("/api/auth",authRoutes)
 app.use("/api/songs",songRoutes)
